b7183cde696a48bd6c943532a2647470e0f0984e
Dan Benson <dbenson3091@gmail.com>
2021-11-11 20:59:20 -0600

    updated javascript with docstrings and rewrote code for readability

--- src/script.js
+++ src/script.js
@@ -4,7 +4,8 @@
 
 const menu = document.querySelector(".menu__items");
 
-//Smooth Scrolling
+/** Function clicks on a link and scroll to the html section, if the classlist doesn't contain "item__resume" */
+
 menu.addEventListener("click", function (e) {
   if (!e.target.classList.contains("item__resume")) e.preventDefault();
 
@@ -15,8 +16,10 @@ menu.addEventListener("click", function (e) {
   }
 });
 
-//Smooth Scrolling through sections
 const sections = document.querySelectorAll("section");
+
+/** Function reveals a new section of the page when a certain point of the 
+ * view port has crossed a specific marker, removes hidden class */
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
@@ -32,7 +35,9 @@ const sectionObserver = new IntersectionObserver(revealSection, {
   threshold: 0.12,
 });
 
-sections.forEach(function (section) {
+for (const section of sections) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
-});
+}
+
+
