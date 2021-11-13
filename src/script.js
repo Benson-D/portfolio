ac98597671ba13711035eaee7473561fa7619fd7
Dan Benson <dbenson3091@gmail.com>
2021-11-13 13:57:09 -0600

    cleaned up javascript code for readability

--- src/script.js
+++ src/script.js
@@ -1,24 +1,23 @@
 "use strict";
 
-//Nav Bar Links
-
 const menu = document.querySelector(".menu__items");
-
-/** Function clicks on a link and scroll to the html section, if the classlist doesn't contain "item__resume" */
-
-menu.addEventListener("click", function (e) {
-  if (!e.target.classList.contains("item__resume")) e.preventDefault();
-
-  //Matching
-  if (e.target.classList.contains("item__link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  }
-});
-
 const sections = document.querySelectorAll("section");
 
-/** Function reveals a new section of the page when a certain point of the 
+/** Function clicks on a link and scrolls smoothly to the html section, if the classlist doesn't contain "item__resume" */
+
+function smoothScrolling(e){
+  if (!e.target.classList.contains("item__resume")) {
+    e.preventDefault();
+
+    //Matching
+    if (e.target.classList.contains("item__link")) {
+      const id = e.target.getAttribute("href");
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+  }
+}
+
+/** Function reveals a new section of the portfolio when a certain point of the 
  * view port has crossed a specific marker, removes hidden class */
 
 const revealSection = function (entries, observer) {
@@ -36,8 +35,8 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 });
 
 for (const section of sections) {
-  sectionObserver.observe(section);
   section.classList.add("section--hidden");
+  sectionObserver.observe(section);
 }
 
-
+menu.addEventListener("click", smoothScrolling);
