d3f37889c53bd79c7105540193b3fe666ff43249
Dan Benson <dbenson3091@gmail.com>
2021-11-28 23:48:37 -0600

    updated images, updated spacing between sections

--- src/project.js
+++ src/project.js
@@ -4,7 +4,7 @@ const projectContainer = [
     {
         name: "Warbler",
         date: "October 2021",
-        img: "./img/warbler.jpg", 
+        img: "./img/warbler.jpeg", 
         demo: "https://warbler-benson.herokuapp.com/",
         detail: "Warbler is an application that is a miniature twitter clone. You can post messages, like and unlike messages. You can follow, unfollow users, and contains a profile page to display info about yourself.", 
         tech: "HTML | CSS | Flask | PostgreSQL",
@@ -13,7 +13,7 @@ const projectContainer = [
     {
         name: "Jobly",
         date: "November 2021",
-        img: "./img/jobly.jpg", 
+        img: "./img/jobly.jpeg", 
         demo: "https://benson-jobly.surge.sh/",
         detail: "A full-stack mock job application. A user can login and signup to the application. A user can search for companies, jobs, and a specific job each company has.", 
         tech: "HTML | CSS | React | Node.js | Express | PostgreSQL",
@@ -22,7 +22,7 @@ const projectContainer = [
     {
         name: "Sharebnb",
         date: "November 2021",
-        img: "./img/sharebnb.jpg", 
+        img: "./img/sharebnb.jpeg", 
         demo: "https://benson-sharebnb.surge.sh/",
         detail: "Sharebnb is an application that is an airbnb clone. You can see listings of current places open for availability, and can post a household for other users to view.", 
         tech: "HTML | CSS | Flask | PostgreSQL | AWS S3 | React | TypeScript",
