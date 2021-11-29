6075b8e367a00b7e4a2f46352b9ac8b9eacc7d49
Dan Benson <dbenson3091@gmail.com>
2021-11-28 23:01:12 -0600

    add a project folder, added a function to display projects more dynamically

---
+++ src/project.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const projectContainer = [
+    {
+        name: "Warbler",
+        date: "October 2021",
+        img: "./img/warbler.jpg", 
+        demo: "https://warbler-benson.herokuapp.com/",
+        detail: "Warbler is an application that is a miniature twitter clone. You can post messages, like and unlike messages. You can follow, unfollow users, and contains a profile page to display info about yourself.", 
+        tech: "HTML | CSS | Flask | PostgreSQL",
+        github: "https://github.com/Benson-D/warbler"
+    },
+    {
+        name: "Jobly",
+        date: "November 2021",
+        img: "./img/jobly.jpg", 
+        demo: "https://benson-jobly.surge.sh/",
+        detail: "A full-stack mock job application. A user can login and signup to the application. A user can search for companies, jobs, and a specific job each company has.", 
+        tech: "HTML | CSS | React | Node.js | Express | PostgreSQL",
+        github: "https://github.com/Benson-D/react-jobly"
+    },
+    {
+        name: "Sharebnb",
+        date: "November 2021",
+        img: "./img/sharebnb.jpg", 
+        demo: "https://benson-sharebnb.surge.sh/",
+        detail: "Sharebnb is an application that is an airbnb clone. You can see listings of current places open for availability, and can post a household for other users to view.", 
+        tech: "HTML | CSS | Flask | PostgreSQL | AWS S3 | React | TypeScript",
+        github: "https://github.com/Benson-D/sharebnb-backend"
+    }
+]
--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 
 const menu = document.querySelector(".menu__items");
 const sections = document.querySelectorAll("section");
+const projectSection = document.getElementById("section__project")
 
 /** Function clicks on a link and scrolls smoothly to the html section, if the classlist doesn't contain "item__resume" */
 
@@ -33,6 +34,46 @@ const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.12,
 });
+
+
+/** Renders an individual project in projects section */
+function renderProject(projects){
+
+  for (const project of projects) {
+
+    const {name, demo, img, date, detail, tech, github} = project
+
+    const html = 
+    `<div class="project__content">
+        <div class="app__layout">
+          <a class="app__link--demo" href="${demo}" target="_blank">
+            <img 
+              class="project__img" src="${img}" 
+              alt="${name.toLowerCase()}-app" />
+            <figurecaption class="app__demo">Live Demo</figurecaption>
+          </a> 
+        </div>
+        <div class="app">
+          <div class="app__header">
+            <h2 class="app__info">${name}</h2>
+            <p class="app__info--date">${date}</p>
+          </div>
+          <p class="app__detail">${detail}</p>
+          <p class="app__detail">
+            <b>Tech stack: </b>${tech}
+          </p>
+          <p class="app__detail">
+            <b>GitHub:</b>
+            <a class="app__link" href="${github}">${name.toLowerCase()} app</a>
+          </p>
+        </div>
+    </div>`
+
+    projectSection.insertAdjacentHTML("beforeend", html);
+  }
+}
+
+renderProject(projectContainer);
 
 for (const section of sections) {
   section.classList.add("section--hidden");
