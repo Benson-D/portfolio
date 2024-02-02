dfc51d09242f822d84d29f36ddf88f7ca18b958a
Dan Benson <dbenson3091@gmail.com>
2024-02-01 21:51:57 -0600

    updated portfolio with new deployed site

--- src/project.js
+++ src/project.js
@@ -18,5 +18,14 @@ const projectContainer = [
         detail: "Pokedex is a frontend application that utilizes the pokemon api. Displays data on various generations, and includes a poke battle game.", 
         tech: "React | TypeScript | Tailwind | GraphQL",
         github: "https://github.com/Benson-D/pokedex"
+    },
+    {
+        name: "Foodie Love",
+        date: "December 2023",
+        img: "./img/gallery1.jpg", 
+        demo: "https://foodielove.vercel.app/",
+        detail: "Foodie Love is an application that stores recipes. You can create recipes, and view other peoples recipes.", 
+        tech: "React | TypeScript | Redux | MUI | Express | Prisma | AWS S3",
+        github: "https://github.com/Benson-D/foodie-love-frontend"
     }
 ]
